9c54ab61a636d1a51b00b10e971187f4afd2960a
Jeffrey Ho <jeff.ho@hotmail.com>
2022-08-05 01:31:19 +0000

    promise version

---
+++ index2.js
@@ -0,0 +1,16 @@
+// const { fetchMyIP } = require('./iss_promised');
+// const {fetchCoordsByIP} = require('./iss_promised');
+// const {fetchISSFlyOverTimes} = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  // .catch((error) => {
+  //   console.log('It didn\'t work: ', error.message)
+  // })
+
+  const printPassTimes = function(passTimes) {
+    console.log(passTimes)
+  }
